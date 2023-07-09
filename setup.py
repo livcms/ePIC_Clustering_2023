b865cc3fa2dbcc1f6c052f42505cf2a63c1a5b6b
livcms <61872243+livcms@users.noreply.github.com>
2023-07-09 14:06:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ePIC_Clustering',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Patrick McCormack',
+      url='https://github.com/wpmccormack/ePIC_Clustering_2023',
+      packages=['distutils', 'distutils.command'],
+     )
